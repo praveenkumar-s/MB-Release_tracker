9bafdcb77163a15642fb041aa43660f3f62ccf95
praveenkumar-s <praveenkumar.s@realimage.com>
2018-08-09 15:07:18 +0530

    Added Highlight to Statuses for better focus

--- process.py
+++ process.py
@@ -20,6 +20,8 @@ def swap_container_class():
 #release_date=json.load(open("release_dates_sample.json"), object_pairs_hook=OrderedDict)
 
 def processor(release_date_json):
+    Highlight_start='<span style="background-color: #FFFF00"><b>'
+    Highlight_end='</span></b>'
     TR=TaigaReader(project_id=release_date_json['product'])
     os.environ['auth_token']= TR.set_Auth_token(os.environ["TAIGA_UN"],os.environ["TAIGA_PWD"])
     config=json.load(open('TaigaConfig.json'))
@@ -29,13 +31,13 @@ def processor(release_date_json):
         for subitems in release_date_json['dates'].get(items):
             if(subitems['kind']=='story'):
                 operation=storytree.StorytTree(subitems['ref'], config['project_ids'][release_date_json['product']])
-                contents_per_date= contents_per_date + html_templates.story_container.format('Story: '+operation.story_ref+': '+operation.description,'Status: '+operation.status ,  'Issues: Open: {0} | Closed: {1}'.format(operation.Pending_issues,operation.closed_issues), 'Tasks:   Open: {0} | Closed: {1}'.format(operation.pending_tasks,operation.closed_tasks))
+                contents_per_date= contents_per_date + html_templates.story_container.format('Story: '+operation.story_ref+': '+operation.description,'Status: '+Highlight_start+operation.status+Highlight_end ,  'Issues: Open: {0} | Closed: {1}'.format(operation.Pending_issues,operation.closed_issues), 'Tasks:   Open: {0} | Closed: {1}'.format(operation.pending_tasks,operation.closed_tasks))
             if(subitems['kind']=='issue'):
                 operation=issuetree.IssueTree(subitems['ref'], config['project_ids'][release_date_json['product']])
-                contents_per_date= contents_per_date + html_templates.issue_containter.format('Issue: '+operation.issue_ref+': '+ operation.issue_desc,'Status: '+operation.issue_status)                
+                contents_per_date= contents_per_date + html_templates.issue_containter.format('Issue: '+operation.issue_ref+': '+ operation.issue_desc,'Status: '+Highlight_start+operation.issue_status+Highlight_end)                
             if(subitems['kind']=='epic'):
                 operation=epictree.EpicTree(subitems['ref'], config['project_ids'][release_date_json['product']])
-                contents_per_date= contents_per_date + html_templates.epic_container.format('Epic: '+operation.epic_ref+': '+operation.epic_desc , 'Status: '+operation.epic_status ,'Stories: Open:{0} | Closed: {1}'.format(int(operation.epic_open_stories),int(operation.epic_closed_stories)))
+                contents_per_date= contents_per_date + html_templates.epic_container.format('Epic: '+operation.epic_ref+': '+operation.epic_desc , 'Status: '+ Highlight_start+operation.epic_status+Highlight_end ,'Stories: Open:{0} | Closed: {1}'.format(int(operation.epic_open_stories),int(operation.epic_closed_stories)))
             
         contents= contents + html_templates.data_container.format(Release_date=items, stories_or_Issues_or_epics= contents_per_date, container_class=swap_container_class())
     contents=html_templates.base_page.replace('{ELEMENTS_OF_TIMELINE}',contents)    
